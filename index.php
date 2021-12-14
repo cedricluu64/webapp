a50739ebb323187086a3dfadcc4cad8eef8053ba
cedricluu64 <96107962+cedricluu64@users.noreply.github.com>
2021-12-14 12:01:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<h1> test </h1>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tester si votre email a était piraté</title>
+</head>
+<body>
+    <table>
+        <form action="" method="post">
+        <tr><th>email</th><td><input type="text" name="email" id="email" value=""/></td></tr>
+        <tr><td><input type="submit" name="valide" id="valide" value="Tester" /></td></tr>
+        </form>
+    </table>    
+    
+</body>
+</html>
